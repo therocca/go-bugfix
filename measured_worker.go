808119da6e2f033a82a23c27a37427dcd4724e72
MSI\rocca <roccamena.massimiliano@gmail.com>
2025-03-31 18:01:49 +0100

    Fixed race conditions inside measured worker test case

--- measured_worker.go
+++ measured_worker.go
@@ -1,15 +1,19 @@
 package main
 
+import (
+	"sync/atomic"
+)
+
 type MeasuredWorker struct {
 	Worker
-	value int
+	value  int32
 }
 
 func (m *MeasuredWorker) Work() {
 	m.Worker.Work()
-	m.value++
+	atomic.AddInt32(&m.value, 1)
 }
 
 func (m *MeasuredWorker) Value() int {
-	return m.value
+	return int(atomic.LoadInt32(&m.value))
 }
